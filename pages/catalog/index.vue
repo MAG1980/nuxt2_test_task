b39df0ad2fd06350534638f269ad7bca1da2db2a
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-06-08 16:27:48 +0300

    Вывел данные о товарах, полученные из API, на страницу catalog.

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="container-lg">
     <h1 @click="decrementCounter">Catalog {{ counter }}</h1>
     <div @click="incrementCounter">+</div>
+    <ProductsList :productsList="productsList"/>
   </div>
 </template>
 
@@ -9,8 +10,18 @@
 import {mapGetters, mapMutations} from 'vuex';
 
 export default {
-  name: "index.vue",
+  name: "index-vue",
   layout: "catalog",
+  data() {
+    return {
+      productsList: {}
+    }
+  },
+  async asyncData({$axios}) {
+
+    const data = await $axios.$get('http://localhost:3000/api/getJSON')
+    return {productsList: data.goods}
+  },
   computed: {
     ...mapGetters(
       {counter: 'counter/getCounter'}
